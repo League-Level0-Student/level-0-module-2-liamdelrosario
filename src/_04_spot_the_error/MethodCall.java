2b9d49ce4eb30f831c2eaf82f950e5f1df91c96f
league <suryanigupta10@gmail.com>
2024-02-18 10:35:29 -0800

    done with this one

--- src/_04_spot_the_error/MethodCall.java
+++ src/_04_spot_the_error/MethodCall.java
@@ -7,9 +7,9 @@ public class MethodCall {
 	// Add ONE line of code to fix this program.
 	
 	public static void main(String[] args) {
-		
+		printMessage("hello world");
 	}
-	String aMessage = ("hello world");
+	
 	static void printMessage(String aMessage) {
 		System.out.println(aMessage);
 	
@@ -17,6 +17,6 @@ public class MethodCall {
 		
 	}
 	
-		
 	
-}
+		
+	}
